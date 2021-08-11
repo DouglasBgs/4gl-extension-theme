a46f1f436d62c02784057f50f0a36310b3c4dce1
Douglas Borges <47727307+DouglasBgs@users.noreply.github.com>
2021-08-10 23:03:16 -0300

    Atualização na criação de repositórios

--- repositorios.js
+++ repositorios.js
@@ -1,5 +1,9 @@
+
+// modificar os repositórios e colocar sempre com duas \ e no fim adicionar mais duas \
 module.exports = {
-  issue_ti: "C:\\Users\\dougl\\Desktop\\Teste repositórios\\",
-  issue_codificacao: "C:\\Users\\dougl\\Desktop\\Teste repositórios\\",
-  evidencia: "C:\\Users\\dougl\\Desktop\\Teste repositórios\\evidencia"
+  issue_ti: "C:\\Users\\dougl\\Desktop\\Teste repositórios\\issue_ti\\",
+  issue_tu: "C:\\Users\\dougl\\Desktop\\Teste repositórios\\issue_tu\\",
+  issue_codificacao: "C:\\Users\\dougl\\Desktop\\Teste repositórios\\issue\\",
+  evidencia: "C:\\Users\\dougl\\Desktop\\Teste repositórios\\evidencia\\",
+  arquivos_romana: "C:\\Users\\dougl\\Desktop\\Teste repositórios\\romana\\"
 };
--- src/extension.js
+++ src/extension.js
@@ -2,28 +2,82 @@
 const vscode = require('vscode');
 const repositorio = require('../repositorios.js');
 const fs = require('fs');
- 
-function  activate(context) {
+const fs2 = require('fs-extra');
+const { time } = require('console');
 
-	let disposable = vscode.commands.registerCommand('extension.create.issue', async function () {
-		const nome_issue = await vscode.window.showInputBox({ prompt: 'Insira o nome da issue', placeHolder: 'Nome issue' });
-		fs.mkdir(repositorio.issue_codificacao+nome_issue, (err) => {
-			if (err) {
-				vscode.window.showInformationMessage('erro na criação'+ err);
-			}
-		});
-		fs.mkdir(repositorio.evidencia+nome_issue, (err) => {
-			if (err) {
-				vscode.window.showInformationMessage('erro na criação'+ err);
-			}
-			vscode.window.showInformationMessage('criado com sucesso');
-		});		
+function criaDiretorio(nomeDiretorio) {
+return fs.mkdir(nomeDiretorio, (err) => {
+		if (err) {
+			return err
+		}
 	});
 
-	context.subscriptions.push(disposable);
 }
 
-function deactivate() {}
+function copiaDiretorio(copiar, destino) {
+	return fs2.copy(copiar, destino, (err) => {
+		if (err) {
+			return err
+		}
+	});
+
+}
+function validaDadosInformados(texto) {
+	return !!texto.match(/[A-Za-z].+\-.+[0-9]/);
+}
+
+function activate(context) {
+
+	let create_issue = vscode.commands.registerCommand('extension.create.issue', async function () {
+		const nome_issue = await vscode.window.showInputBox({ prompt: 'Insira o nome da issue de codificação', placeHolder: 'Nome issue' });
+		if (validaDadosInformados(nome_issue)) {
+			try {
+				 criaDiretorio(repositorio.issue_codificacao + nome_issue)
+				 criaDiretorio(repositorio.evidencia + nome_issue)
+				return vscode.window.showInformationMessage('Issue de codificação: ' + nome_issue + ' criado com sucesso');
+			} catch (err) {
+				return vscode.window.showErrorMessage('erro na criação' + err);
+			};
+		} else {
+			vscode.window.showInformationMessage('dados ' + nome_issue + ' Inválidos');
+
+		}
+
+	});
+
+	let create_issue_tu = vscode.commands.registerCommand('extension.create.issue_tu', async function () {
+		const nome_issue = await vscode.window.showInputBox({ prompt: 'Insira o nome da issue de Teste Unitário', placeHolder: 'Nome issue' });
+		if (validaDadosInformados(nome_issue)) {
+			try {
+				await criaDiretorio(repositorio.issue_tu + nome_issue)				
+				await copiaDiretorio(repositorio.arquivos_romana + nome_issue, repositorio.issue_tu + nome_issue)
+				return vscode.window.showInformationMessage('Issue de teste Unitário: ' + nome_issue + ' criado com sucesso');
+			} catch (err) {
+				return vscode.window.showErrorMessage('erro na criação' + err);
+			}
+		} else {
+			vscode.window.showInformationMessage('dados informados são inválidos!');
+
+		}
+	});
+	let create_issue_ti = vscode.commands.registerCommand('extension.create.issue_ti', async function () {
+		const nome_issue = await vscode.window.showInputBox({ prompt: 'Insira o nome da issue de Teste Integrado', placeHolder: 'Nome issue' });
+		if (validaDadosInformados(nome_issue)) {
+			try {
+				await criaDiretorio(repositorio.issue_ti + nome_issue)
+				await copiaDiretorio(repositorio.arquivos_romana + nome_issue, repositorio.issue_ti + nome_issue)
+				return vscode.window.showInformationMessage('Issue de teste Integrado: ' + nome_issue + ' criado com sucesso');
+			} catch (err) {
+				return vscode.window.showErrorMessage('erro na criação' + err);
+			}
+		} else {
+			vscode.window.showInformationMessage('dados informados são inválidos!');
+		}
+	});
+	context.subscriptions.push(create_issue, create_issue_tu,create_issue_ti);
+}
+
+function deactivate() { }
 
 module.exports = {
 	activate,

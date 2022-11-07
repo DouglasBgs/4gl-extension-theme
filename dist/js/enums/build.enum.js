51c70599ed77b999ecf7fa066285c7cbcd1dbcaa
DouglasBgs <borgesdoga@gmail.com>
2022-11-07 10:22:23 -0300

    remover dist

--- dist/js/enums/build.enum.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Build = void 0;
-var Build;
-(function (Build) {
-    Build["b64"] = "Build 64";
-    Build["b32"] = "Build 32";
-})(Build = exports.Build || (exports.Build = {}));
-//# sourceMappingURL=build.enum.js.map

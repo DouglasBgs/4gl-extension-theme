db7bb3ab95c404d635d48fa37b2cae3d661db367
Douglas Borges <47727307+DouglasBgs@users.noreply.github.com>
2023-04-18 17:32:11 -0300

    Alterações na inicialização do tomcat

--- app/controllers/servers.ts
+++ app/controllers/servers.ts
@@ -4,44 +4,50 @@ import { Utils } from '../utils/utils.js'
 import { Build } from '../enums/build.enum'
 
 export class Servers {
-  public static async openAppServer (pathToAppServer: string) {
+  public static async openAppServer(pathToAppServer: string) {
     const config = new ConfigModel(pathToAppServer)
     const build: string = await this.selectBuild()
     if (!build) { return }
     let appserver: string
-
+    let nameAppServer: string
     if (build == Build.b64) {
       appserver = config.data.appserver_64
+      nameAppServer = 'AppServer 64'
     } else {
       appserver = config.data.appserver
+      nameAppServer = 'AppServer'
     }
-    this.startCommand(appserver, 'AppServer')
+    this.startCommand(appserver, nameAppServer)
   }
 
-  public static async openTss (pathToTss: string) {
+  public static async openTss(pathToTss: string) {
     const config = new ConfigModel(pathToTss)
     const build: string = await this.selectBuild()
     if (!build) { return }
     let tss: string
     let dbAcessTss: string
+    let nameTss: string
+    let nameDbAccess: string
     if (build == Build.b64) {
-      tss = config.data.tss
-      dbAcessTss = config.data.dbacess_tss
+      tss = `${config.data.tss} -console`
+      nameTss = 'TSS 64'
+      nameDbAccess = 'DBAcess TSS 64'
+      dbAcessTss = `${config.data.dbacess_tss} -console`
     } else {
-      tss = config.data.tss_64
-      dbAcessTss = config.data.dbacess_tss_64
+      tss =  `${config.data.tss_64} -console`
+      nameTss = 'TSS'
+      nameDbAccess = 'DBAcess TSS'
+      dbAcessTss =`${config.data.dbacess_tss_64} -console`
     }
     this.startCommand(tss, nameTss)
     this.startCommand(dbAcessTss, nameDbAccess)
   }
 
   public static async openTomcatDatasul(pathToTomcat: string) {
-
-    
     let tomcat: string =`
-     ${pathToTomcat}scripts\\inicia.lnk
-     exit`
-    let nameTomcat: string = 'TOMCAT'
+    cd ${pathToTomcat}
+    ${pathToTomcat}\\bin\\catalina.bat run Using CATALINA_BASE:"${pathToTomcat}" Using CATALINA_HOME:   "${pathToTomcat}" Using CATALINA_TMPDIR: "${pathToTomcat}temp"`
+    let nameTomcat: string = 'Tomcat Datasul'
     this.startCommand(tomcat, nameTomcat)
     
   }
@@ -63,7 +69,7 @@ export class Servers {
       window.createTerminal(name, 'C:\\Windows\\system32\\cmd.exe')
       window.terminals.findIndex((terminal) => {
         if (terminal.name == name) {
-          terminal.sendText(`${command} -console`)
+          terminal.sendText(`${command} `)
         }
       })
       Utils.MostraMensagemInfo(`${name} aberto com sucesso`)

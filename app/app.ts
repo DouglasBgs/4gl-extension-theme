45f58df84f9b390a8494a9778681189288e9f946
Douglas Borges <47727307+DouglasBgs@users.noreply.github.com>
2023-12-05 13:47:43 -0300

    Adicionando compilador

--- app/app.ts
+++ app/app.ts
@@ -7,42 +7,47 @@ import { WebviewFile } from './controllers/webview'
 import {ExtensionContext}  from 'vscode'
 import { buildManual } from './controllers/buildManual'
 import { Repositorio } from "./enums/repositorio.enum";
+import { Compiler } from "./controllers/compiler";
 
 function activate(context: ExtensionContext) {
     const pathToDist = join(context.extensionPath);
     const pathToConfig = join(pathToDist, ".config.json");
 
-    commands.registerCommand("extension.open.appserver", function () {
+    commands.registerCommand("MTAux.open.appserver", function () {
         Servers.openAppServer(pathToConfig);
     });
-    commands.registerCommand("extension.open.tss", function () {
+    commands.registerCommand("MTAux.open.tss", function () {
         Servers.openTss(pathToConfig);
     });
-    commands.registerCommand("extension.create.issue", function () {
+    commands.registerCommand("MTAux.create.issue", function () {
         Issues.createLogix(pathToConfig);
     });
-    commands.registerCommand("extension.open.arquivo_log", function () {
+    commands.registerCommand("MTAux.open.arquivo_log", function () {
         Servers.openLogFile(pathToConfig);
     });
-    commands.registerCommand("extension.download.rpo", function () {
+    commands.registerCommand("MTAux.download.rpo", function () {
         RpoController.download(pathToConfig);
     });
-    commands.registerCommand("extension.open.Webview", function () {
+    commands.registerCommand("MTAux.open.Webview", function () {
         WebviewFile.open(pathToConfig, context);
     });
-    commands.registerCommand("extension.issue.datasul", function () {
+    commands.registerCommand("MTAux.issue.datasul", function () {
         Issues.createDatasul(pathToConfig);
     });
 
-    commands.registerCommand("extension.buildManual", function () {
+    commands.registerCommand("MTAux.buildManual", function () {
         buildManual.novo(pathToConfig);
     });
 
-    commands.registerCommand("extension.tomcat", function () {
+    commands.registerCommand("MTAux.tomcat", function () {
         Servers.openTomcat(pathToConfig);
     });
-    commands.registerCommand("extension.server.ems2", function () {
+    commands.registerCommand("MTAux.server.ems2", function () {
         Issues.createDatasul(pathToConfig, Repositorio.Ems2);
+    });
+
+    commands.registerCommand("MTAux.compiler", function () {
+        Compiler.compile();
     });
 }
 
